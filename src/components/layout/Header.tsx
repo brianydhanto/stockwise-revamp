d20df45d9bdd7c969079f67faac13cdde7b363eb
brianydhanto <brianydhanto@gmail.com>
2025-09-24 23:09:28 +0700

    Fix header size

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -2,11 +2,13 @@
 export const Header = () => {  
   return (
     <>
-      <div className="floating-bar mx-auto justify-self-center gap-4">
-        <div className="self-center text-xl cursor-pointer">STOCKWISE</div>
-        <div className="self-center flex gap-6">
-          <div>
-            <button className="btn-primary cursor-pointer text-nowrap">JOIN NOW</button>
+      <div className="w-full flex justify-center">
+        <div className="floating-bar gap-4">
+          <div className="self-center text-xl cursor-pointer">STOCKWISE</div>
+          <div className="self-center flex gap-6">
+            <div>
+              <button className="btn-primary cursor-pointer text-nowrap">JOIN NOW</button>
+            </div>
           </div>
         </div>
       </div>
